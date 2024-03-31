56b1e54be43635a1d957ec6761032d3844cb39de
abdalazeez <abdalazeez@aeliasoft.com>
2024-03-31 23:43:25 +0300

    add .env.example file and connect server with iot cloud

--- app.js
+++ app.js
@@ -1,27 +1,63 @@
-import express from 'express'
-import { ArduinoIoTCloud } from 'arduino-iot-js';
+import express, { json } from 'express'
+import IotApi from '@arduino/arduino-iot-client';
+import rp from 'request-promise';
+import cors from 'cors';
+
 const PORT = process.env.PORT || 5000;
 const app = express();
-    (async () => {
-        const client = await ArduinoIoTCloud.connect({
-            clientId: 'danSN2bJdS7v1ERWEtGr9oP76YVaJswsnjAAYAh1s6SWtjlEF8Zgr7oOq7p0KCzQ',
-            clientSecret: 'danSN2bJdS7v1ERWEtGr9oP76YVaJswsnjAAYAh1s6SWtjlEF8Zgr7oOq7p0KCzQ',
-            onDisconnect: (message) => console.error(message),
-        });
+app.use(json())
+app.use(cors())
 
-        // Send a value to a thing property
-        const value = 'some value';
-        client.sendProperty('YOUR_THING_ID', 'YOUR_VARIABLE_NAME', value);
+async function getToken() {
+  var options = {
+    method: 'POST',
+    url: 'https://api2.arduino.cc/iot/v1/clients/token',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    json: true,
+    form: {
+      grant_type: 'client_credentials',
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      audience: 'https://api2.arduino.cc/iot'
+    }
+  };
 
-        // Listen to a thing property's changes
-        client.onPropertyValue('YOUR_THING_ID', 'ANOTHER_VARIABLE_NAME', (value) => console.log(value));
+  try {
+    const response = await rp(options);
+    return response['access_token'];
+  }
+  catch (error) {
+    console.error("Failed getting an access token: " + error)
+  }
+}
 
-    })();
-    app.get('/', (req, res) => {
-        res.send('Hello, World!');
-      });
-      
-      // Start the server
-      app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-      });
+let result = [];
+
+async function run() {
+  var client = IotApi.ApiClient.instance;
+  var oauth2 = client.authentications['oauth2'];
+  oauth2.accessToken = await getToken();
+
+  // variables API
+  var api = new IotApi.PropertiesV2Api(client);
+  
+  // thing id -> test
+  api.propertiesV2List('9e25f54c-2c6e-404e-9081-6585edab3230').then(properties => {
+    result = properties.map(prop => {
+      return { name: prop.name, value: prop.last_value }
+    })
+    console.log(properties);
+  }, error => {
+    console.log(error)
+  });
+}
+
+run();
+
+app.get('/', (req, res) => {
+  res.send(result).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
